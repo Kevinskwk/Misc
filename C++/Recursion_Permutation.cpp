245a32b737f166265b5eb13c85f28011e9785ad7
Kevinskwk <kevinskwk@gmail.com>
2019-08-31 14:50:34 +0800

    update Recursion permutation algorithm

--- C++/Recursion_Permutation.cpp
+++ C++/Recursion_Permutation.cpp
@@ -1,9 +1,11 @@
+//An algorithm to get permutation using recursion
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-int cnt(0);
-void permute(char a[], int i, int n) {
+int cnt(0); //number of permutation
+void permute(char a[], int i, int n) { //arguments: array of chars, starting index, ending index
     int j;
     if (i == n) {
         cout << a <<endl;
@@ -21,7 +23,7 @@ void permute(char a[], int i, int n) {
 
 int main() {
     char a[] = "ABCD";
-    permute(a, 1, 2);
+    permute(a, 0, 3);
     cout << cnt << endl;
     return 0;
 }
